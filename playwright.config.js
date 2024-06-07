0bc4be3c1e165cc0ddc2e5227792a3a21684131a
vgulve <vikasgulve1999@gmail.com>
2024-06-07 16:35:17 +0530

    Add files via upload

---
+++ playwright.config.js
@@ -0,0 +1,34 @@
+// playwright.config.js
+// @ts-check
+const { defineConfig, devices } = require('@playwright/test');
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+module.exports = defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    
+    
+  ],
+
+  /* Run your local dev server before starting the tests */
+  // Uncomment and adjust if needed
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://127.0.0.1:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
+});
